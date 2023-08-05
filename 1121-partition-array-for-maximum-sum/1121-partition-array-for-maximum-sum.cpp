c6b3a4256e5ad9f79bce09075c99c2697c4fd6e3
Aryan Gupta <aryangupta27.2002@gmail.com>
2023-08-05 16:47:58 +0530

    Time: 11 ms (93.62%), Space: 8.2 MB (99.74%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,36 +1,27 @@
 class Solution {
-private:
-    int f(vector<int>& arr, int k, int n, int ind, vector<int> &dp){
-        if(ind==n) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int len=0;
-        int maxi=INT_MIN;
-        int max_len=INT_MIN;
-        for(int i=ind; i<min(n, ind+k); i++){
-            maxi=max(maxi, arr[i]);
-            len++;
-            max_len=max(max_len, len*maxi+f(arr, k, n, i+1, dp));
-        }
-        return dp[ind]=max_len;
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n=arr.size();
-        
-        vector<int> dp(n+1, 0);
-        //return f(arr, k, n, 0, dp);
-        dp[n]=0;
-        for(int j=n-1; j>=0; j--){
-            int len=0;
-            int maxi=INT_MIN;
-            int max_len=INT_MIN;
-            for(int i=j; i<min(n, j+k); i++){
-                maxi=max(maxi, arr[i]);
-                len++;
-                max_len=max(max_len, len*maxi+dp[i+1]);
-            }
-        dp[j]=max_len;
+        int m = 0;
+        std::array<int, 501> arrMaxSum{0};
+        for (int i = 0; i < k; ++i)
+        {
+            m = (arr[i] > m) ? arr[i] : m;
+            arrMaxSum[i] = m * (i + 1);
         }
-        return dp[0];
+        int n = arr.size();
+        int maxSub = 0;
+        int maxTempSum = 0;
+        for (int i = k; i < n; ++i)
+        {
+            maxSub = arr[i];
+            maxTempSum = 0;
+            for (int j = 0; j < k; ++j)
+            {
+                maxSub = (arr[i - j] > maxSub) ? arr[i - j] : maxSub;
+                maxTempSum = arrMaxSum[i - j - 1] + maxSub * (j + 1);
+                arrMaxSum[i] = (maxTempSum > arrMaxSum[i]) ? maxTempSum : arrMaxSum[i];
+            }
+        }
+        return arrMaxSum[n - 1];
     }
-};
+}; 
