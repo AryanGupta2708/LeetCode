463a6ff5a061d2547bd7f3c85f1adc2c8a5c516f
Aryan Gupta <aryangupta27.2002@gmail.com>
2023-09-23 02:38:36 +0530

    Time: 15 ms (28.33%), Space: 7.6 MB (6.9%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-         unordered_map<int,vector<int>> m;
-        int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-            m[s[i]].push_back(i);
-        }
-        int ans=0;
-        for(auto it:m)
-        {
-            
-            int sz=it.second.size();
-            int i=0,j=0;
-            while(j<sz)
-            {
-
-                while(it.second[j]-it.second[i]-(j-i)>k) i++;
-                int extra=k-(it.second[j]-it.second[i]-(j-i));
-                // cout<<extra<<" ";
-                int curr=it.second[j]-it.second[i]+1;
-
-                if(it.second[i]>0)
-                {
-                    curr+=min(it.second[i],extra);
-                    extra-=min(it.second[i],extra);
+        unordered_map<char,int> mp;
+        int left=0,maxCount=0,res=0;
+        for(int right=0;right<s.size();right++){
+            mp[s[right]]++;
+            maxCount=max(maxCount,mp[s[right]]);
+            if(right-left+1-maxCount>k){
+                mp[s[left]]--;
+                left++;
+                if(mp[left]==0){
+                    mp.erase(mp[left]);
                 }
-                if(it.second[j]<n)
-                {
-                    curr+=min(n-it.second[j]-1,extra);
-                    extra-=min(n-it.second[j]-1,extra);
-                }
-                // cout<<it.second[i]<<"->"<<it.second[j]<<" "<<endl;
-                ans=max(ans,curr);
-                j++;
             }
-            
+            else{
+                res=max(res,right-left+1);
+            }
         }
-        return ans;
+        return res;
     }
 };
