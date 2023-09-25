0bdf55b587e02ca6cc7ac3ff00e2f2f80263c0ed
Aryan Gupta <aryangupta27.2002@gmail.com>
2023-09-25 05:36:31 +0530

    Time: 0 ms (100%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int> x;
-        for(int i=0;i<s.length();i++){
-            x[s[i]]++;
-        }
-
-        for(int i=0;i<t.length();i++){
-            x[t[i]]--;
-            if(x[t[i]]<0)
-            return t[i];
-        }
-
-        return 'a';
-        
+        for(int i = 0; i < s.size(); i++) t[i+1] += t[i] - s[i];
+        return t[t.size()-1];
     }
 };
