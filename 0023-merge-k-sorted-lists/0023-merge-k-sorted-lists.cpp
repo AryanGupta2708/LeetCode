4b1406384c11344e94e5ea93a306c10cbcc789d4
Aryan Gupta <aryangupta27.2002@gmail.com>
2023-09-28 14:32:18 +0530

    Time: 187 ms (15.35%), Space: 13.9 MB (34.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,41 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-private:
 public:
-	ListNode* mergeKLists(vector<ListNode*>& lists) {
-		ListNode *lst, *node, *curr_lst;
-		curr_lst = lst = node = nullptr;
-		
-		while ( (node=getNextLowest(lists)) ){
-			if (!lst){
-				lst = curr_lst = node;
-			} else {
-				curr_lst->next = node;
-				curr_lst = curr_lst->next;
-			}
-		}
-		if (curr_lst)
-			curr_lst->next = nullptr;
-		
-		return lst;
-	}
-
-	ListNode* getNextLowest(vector<ListNode*>& lists){
-		int curr_lowest = INT_MAX, curr_idx = -1;
-		for (int i = 0; i < lists.size(); i++){
-			for (ListNode* cursor = lists[i]; cursor; cursor = cursor->next){
-				if (cursor->val < curr_lowest){
-					curr_idx = i;
-					curr_lowest = cursor->val;
-					break;
-				}
-			}
-		}
-
-		if (curr_idx != -1){
-			ListNode* res = lists[curr_idx];
-			lists[curr_idx] = lists[curr_idx]->next;
-			return res;
-		}
-		return nullptr;
-	}
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+    if(lists.empty()){
+        return nullptr;
+    }
+    while(lists.size() > 1){
+        lists.push_back(mergeTwoLists(lists[0], lists[1]));
+        lists.erase(lists.begin());
+        lists.erase(lists.begin());
+    }
+    return lists.front();
+}
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if(l1 == nullptr){
+        return l2;
+    }
+    if(l2 == nullptr){
+        return l1;
+    }
+    if(l1->val <= l2->val){
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    }
+    else{
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
 };
