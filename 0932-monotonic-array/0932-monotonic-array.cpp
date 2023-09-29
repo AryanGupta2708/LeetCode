1b781e5260aad46c97ab2ce497d970686c972568
Aryan Gupta <aryangupta27.2002@gmail.com>
2023-09-29 07:39:10 +0530

    Time: 136 ms (15.96%), Space: 96.8 MB (13.85%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+
+        int f1 = 1,f2 = 1;
+
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<nums[i+1])
+            f1 = 0;
+            if(nums[i]>nums[i+1])
+            f2 = 0;
+        }
+
+        return (f1 || f2) ? true : false;
+        
+    }
+};
